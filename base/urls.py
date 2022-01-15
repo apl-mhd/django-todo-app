2fa11bd25807de1e421e6f27a8ecadd8c3b3c164
Apel Mahmud <44221242+apl-mhd@users.noreply.github.com>
2022-01-15 10:46:25 +0600

    add login logout

--- base/urls.py
+++ base/urls.py
@@ -1,12 +1,16 @@
 from unicodedata import name
 from django.urls import path
 from . import views
-from . views import TaskDetail, TaskList, TaskCreate
+from . views import TaskDetail, TaskList, TaskCreate, TaskUpdate, TaskDelete
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
     path('task-create', TaskCreate.as_view(), name='task-create'),
+    path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
+    path('task-delete/<int:pk>/', TaskDelete.as_view(), name='task-delete'),
+    
+
 
     
 
--- base/views.py
+++ base/views.py
@@ -1,4 +1,4 @@
-from dataclasses import field
+from dataclasses import field, fields
 import imp
 from msilib.schema import ListView
 from multiprocessing import context
@@ -7,7 +7,7 @@ from statistics import mode
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from . models import Task
 
@@ -25,3 +25,15 @@ class TaskCreate(CreateView):
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('tasks')
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
+
