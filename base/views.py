b53826f0c07f1eda932e46c492c221ba2b12cc1f
Apel Mahmud <44221242+apl-mhd@users.noreply.github.com>
2022-01-15 13:25:35 +0600

    Add Registration

--- base/views.py
+++ base/views.py
@@ -4,15 +4,17 @@ from msilib.schema import ListView
 from multiprocessing import context
 from pyexpat import model
 from statistics import mode
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.urls import reverse_lazy
 from . models import Task
 
 from django.contrib.auth.views import LoginView, LogoutView 
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 class CustomLoginView(LoginView):
     template_name = 'base/login.html'
@@ -21,6 +23,27 @@ class CustomLoginView(LoginView):
 
     def get_success_url(self):
         return reverse_lazy('tasks')
+
+
+class RegisterPage(FormView):
+    template_name = 'base/register.html'
+    form_class = UserCreationForm
+    redirect_authenticate_user = True
+    success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
+    
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('tasks')
+        
+        return super(RegisterPage, self).get(*args, **kwargs)
+
+
 
 
 class TaskList(LoginRequiredMixin,ListView):
