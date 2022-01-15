739b03aeb354f7cf2b66ee379a9b83f4866e38f7
Apel Mahmud <44221242+apl-mhd@users.noreply.github.com>
2022-01-15 10:27:17 +0600

    add create view

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,13 @@
 from unicodedata import name
 from django.urls import path
 from . import views
+from . views import TaskDetail, TaskList, TaskCreate
 
 urlpatterns = [
-    path('', views.taskList, name='tasks'),
+    path('', TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+    path('task-create', TaskCreate.as_view(), name='task-create'),
+
+    
+
 ]
--- base/views.py
+++ base/views.py
@@ -1,7 +1,27 @@
+from dataclasses import field
+import imp
+from msilib.schema import ListView
+from multiprocessing import context
+from pyexpat import model
+from statistics import mode
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from . models import Task
+
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'base/task.html'
 
 
-def taskList(request):
-    return HttpResponse("Apel Mahmud")
-
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
